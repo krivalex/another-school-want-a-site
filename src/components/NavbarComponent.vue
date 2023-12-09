94dfc1b7c2c8fd335f4e169e0d0a348c70c72b96
krivalex <new.krivalex@gmail.com>
2023-12-09 07:25:29 +0600

    add dialogs and modals

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -4,18 +4,36 @@
       <router-link to="/" class="navbar-brand">
         <img src="../assets/logo.png" alt="logo" />
       </router-link>
+      <p-button
+        class="enter"
+        v-if="!user"
+        @click="googleRegister"
+        label="Войти"
+        icon="pi pi-sign"
+      ></p-button>
+      <p-button
+        class="enter"
+        v-if="user"
+        @click="googleLogout"
+        label="Выйти"
+        icon="pi pi-sign-out"
+      ></p-button>
     </div>
   </nav>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PButton from 'primevue/button'
+import { useUser } from '@/composables/useUser'
+
+const { user, googleLogout, googleRegister } = useUser()
+</script>
 
 <style scoped>
 .navbar {
   position: relative;
   width: 100%;
   height: 60px;
-  line-height: 60px;
   background-color: #f5f5f5;
   color: black;
   text-align: center;
@@ -26,20 +44,21 @@
   justify-content: space-between;
   align-items: center;
   width: 100%;
+  height: inherit;
 }
 
 .navbar-brand {
   display: inline-block;
   padding: 0 15px;
   font-size: 18px;
-  line-height: 60px;
   white-space: nowrap;
   text-decoration: none;
   color: #333;
 }
 
 .navbar-brand img {
-  height: 60px;
+  height: 50px;
+  margin: auto auto;
 }
 
 .navbar-menu {
@@ -66,5 +85,15 @@
 
 .navbar-item:hover {
   color: #000;
+}
+
+.enter {
+  background-color: #818cf8;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  padding: 0.5rem 1rem;
+  font-size: 1.2rem;
+  margin-right: 1rem;
 }
 </style>
